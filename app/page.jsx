84ee964fad0de9022d4015c6dc440fcc96cfc402
Rahul Kumar <143000302+rkumar2002@users.noreply.github.com>
2024-08-11 03:38:22 +0530

    responsive hompepage & middleware update

--- app/page.jsx
+++ app/page.jsx
@@ -10,9 +10,9 @@ export default function Home() {
   
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center main">
-      <div className="card flex flex-col justify-center items-center z-20 p-32">
-        <div className="my-12 font-medium font-serif  text-7xl z-10"><span className="ai text-8xl">AI </span><span className="text-slate-700">MOCK INTERVIEW</span></div>
-          <Button className="w-52 py-6 rounded-full" onClick={()=>router.push('/dashboard')}>Let's Begin</Button>
+      <div className="card flex flex-col justify-center items-center z-20 p-8 md:p-32">
+        <div className="my-5 font-medium font-serif z-10 text-3xl md:text-5xl lg:text-7xl my-12 text-nowrap"><span className="ai text-4xl md:text-6xl lg:text-8xl">AI </span><span className="text-slate-700">MOCK INTERVIEW</span></div>
+          <Button className="md:w-52 py-6 rounded-full" onClick={()=>router.push('/dashboard')}>Let's Begin</Button>
       </div>
     </div>
   );
